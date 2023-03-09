bc00ac47c0a32e5ae5c45a5807649d2b7696e857
taha soumbate <SOUMBATE.TAHA@ofppt-edu.ma>
2023-03-09 10:43:01 +0100

    style

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ let error2 = document.getElementsByTagName("small")[1];
 let error3 = document.getElementsByTagName("small")[2];
 let error4 = document.getElementsByTagName("small")[3];
 error.innerText = "";
-error.style = "color:red";
+error.style = "color:#e74c3c";
 
 function usernameValid(username) {
   if (username.value.length >= 3) {
-    username.style = "border:2px solid green";
+    username.style = "border:2px solid #2ecc71";
     localStorage.setItem("username", username.value);
   } else {
-    username.style = "border:2px solid red";
+    username.style = "border:2px solid #e74c3c";
     error.innerText = "Username must have be at least 3 characters!";
     setTimeout(() => {
       error.innerText = "";
@@ -24,15 +24,15 @@ function usernameValid(username) {
 }
 
 error2.innerText = "";
-error2.style = "color:red";
+error2.style = "color:#e74c3c";
 function emailValid(email) {
   const testEmail =
     /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   if (email.value.match(testEmail)) {
-    email.style = "border:2px solid green";
+    email.style = "border:2px solid #2ecc71";
     localStorage.setItem("email", email.value);
   } else {
-    email.style = "border:2px solid red";
+    email.style = "border:2px solid #e74c3c";
     error2.innerText = "Email is not Valid!";
     setTimeout(() => {
       error2.innerText = "";
@@ -40,13 +40,13 @@ function emailValid(email) {
   }
 }
 error3.innerText = "";
-error3.style = "color:red";
+error3.style = "color:#e74c3c";
 function passwordValid(password) {
   if (password.value.length >= 3) {
-    password.style = "border:2px solid green";
+    password.style = "border:2px solid #2ecc71";
     localStorage.setItem("password", password.value);
   } else {
-    password.style = "border:2px solid red";
+    password.style = "border:2px solid #e74c3c";
     error3.innerText = "Password must have be at least 3 characters!";
     setTimeout(() => {
       error3.innerText = "";
@@ -54,13 +54,13 @@ function passwordValid(password) {
   }
 }
 error4.innerText = "";
-error4.style = "color:red";
+error4.style = "color:#e74c3c";
 function password2Valid(password2) {
   if (password2.value === password.value && password2.value.length >= 1) {
-    password2.style = "border:2px solid green";
+    password2.style = "border:2px solid #2ecc71";
     localStorage.setItem("password2", password2.value);
   } else {
-    password2.style = "border:2px solid red";
+    password2.style = "border:2px solid #e74c3c";
     error4.innerText = "Passzord don't match";
     setTimeout(() => {
       error4.innerText = "";
